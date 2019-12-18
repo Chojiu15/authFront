43f8bc03992ae9b5a7f4cd7f407e64e4c1b9bd3f
chojiu <yacine@wildcodeschool.fr>
2019-12-18 21:18:01 +0100

    init

---
+++ front/src/components/UserFunctions.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+export const register = newUser => {
+    return axios.post('http://localhost:3000/users/register', {
+        firt_name : newUser.firt_name,
+        last_name : newUser.last_name,
+        email : newUser.email,
+        password : newUser.password
+    })
+    .then(res => console.log('Registred'))
+    .catch(err => console.log(err))
+}
+
+export const login = user => {
+    return axios.post('http://localhost:3000/users/login', {
+        email : user.email,
+        password : user.password
+    })
+    .then(res => {
+        localStorage.setItem('usertoken', res.data)
+        return res.data
+    }) 
+    .catch(err => console.error(err))
+}
